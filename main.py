3b91bcdf0c1a9ceb9410551b68c227137da98f64
Anton <91902160+TeleginAnton@users.noreply.github.com>
2021-12-25 01:08:02 +0300

    version 1 main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import json
-import re
 import random
-import faker
 
 from faker import Faker
-from collections import OrderedDict
+from pprint import pprint
 
 from conf import MODEL
 
@@ -12,118 +10,105 @@ BOOK_JSON = "book.json"
 BOOK_TXT = "books.txt"
 
 
-def _main():
+def _main() -> None:
     """
-    Заполнить после оформления!
-    :return:
+    Формирует список из 100 книг (список словарей) и записывает в файл book.json только один словарь :(
     """
-    # print(book_dict())
-    # print(next(year()))
-    # print(next(pages()))
-    # print(next(isbn13()))
-    # print(next(rating()))
-    # print(next(price()))
-    # print(next(author()))
-    # with open(BOOK_JSON, "w") as function:
-    #     json.dump(python_object, function, indent=5, ensure_ascii=False)
+    python_object = generator()
+    with open(BOOK_JSON, "w") as function:
+        # for word in python_object:
+        #     function.write(word + '\n')
+        json.dump(python_object, function, indent=4, ensure_ascii=False)
 
 
-def book_dict():  # todo застрял тут( Можно подсказку?
-    """
-    Заполнить после оформления!
-    :return:
-    """
-    def decorator(f):
-        ...
-
-        def wrapper(*args):
-            ...
-
-            return
-
-        return wrapper
-
-    return decorator
+def pk_():
+    pk = 1
+    while pk <= 100:
+        yield pk
+        pk += 1
+    return pk
 
 
-# todo Андрей Александрович, как избавиться от переноса строки? Нужен генератор или генератор словарей?
-@book_dict()
-def title() -> dict:
+def title() -> str:
     """
     Функция - генератор.
     :return: Названием книги - генерируемый объект.
     """
     with open(BOOK_TXT, encoding='utf8') as fn:
-        key = ["model"]
-        str_py = fn.readlines()
-        random_str = random.choices(str_py)
-        res = {i: x for i, x in zip(key, random_str)}
-    yield res
+        enum_book = fn.read().splitlines()
+        random_str = random.choice(enum_book)
+    return random_str
 
 
-@book_dict()
 def year() -> int:
     """
     Функция - генератор.
     :return: Год издания - генерируемый объект.
     """
     num_four_signs = (random.randrange(2000, 2021, 6))
-    yield num_four_signs
+    return num_four_signs
 
 
-@book_dict()
 def pages() -> int:
     """
     Функция - генератор.
     :return: Количество страниц - генерируемый объект.
     """
     num_three_signs = (random.randint(100, 200))
-    yield num_three_signs
+    return num_three_signs
 
 
-# todo не смог вызвать метод fake.isbn10(). В документации нашёл такой выход.
-#  Почему в yield приходится прописывать next?
-@book_dict()
-def isbn13() -> int:
+def isbn13() -> None:
     """
     Функция - генератор.
     :return: Международный книжный номер - генерируемый объект.
     """
     fake = Faker()
     book_num = (fake.numerify(text='%%%-%-%%%%%-%%%-%') for _ in range(5))
-    yield next(book_num)
+    return next(book_num)
 
 
-@book_dict()
 def rating() -> int:
     """
     Функция - генератор.
     :return: Рейтинг - генерируемый объект.
     """
     int_nun = (random.randint(0, 5))
-    yield int_nun
+    return int_nun
 
 
-@book_dict()
 def price() -> float:
     """
     Функция - генератор.
     :return: Стоимость книги - генерируемый объект.
     """
     float_num = float(random.randint(0, 9000))
-    yield float_num
+    return float_num
 
 
-@book_dict()
-def author() -> str:
+def author() -> None:
     """
     Функция - генератор.
     :return: Автор - генерируемый объект.
     """
-    author_count = 1
     fake = Faker()
-    author_gen = (fake.name() for _ in range(author_count))
-    yield next(author_gen)
+    one_author = (fake.name())
+    return one_author
+
+
+def fields() -> dict:
+    new_fields = {"title": title(), "year": year(),
+                  "pages": pages(), "isbn13": isbn13(),
+                  "rating": rating(), "price": price(), "author": author()}
+    return new_fields
+
+
+def generator():
+    model = MODEL
+    for i in pk_():
+        res = {"model": model, "pk": i, "fields": fields()}
+        pprint(res, sort_dicts=False)
+    return res
 
 
 if __name__ == '__main__':
