b1396b60eb91687335ab052f0225dffa739cec0c
Anton <91902160+TeleginAnton@users.noreply.github.com>
2022-01-09 12:49:27 +0300

    corrections task1.py

--- task1.py
+++ task1.py
@@ -1,27 +1,17 @@
+import random
+
+BOOK_TXT = "books.txt"
+
+
 def _main():
     print(title())
 
 
-def decorator(fn):
-    max_len_book = 87
-
-    def wrapper(*args):
-        result = fn(args)
-        if result > max_len_book:
-            raise ValueError(f"длина книги больше заданого условия max_len_book: {max_len_book}")
-
-        return result
-
-    return wrapper
-
-
-@decorator
-def title(fn):
-    with open("books.txt", encoding="utf8") as fn:
-        len_book = fn.readlines()
-        max_str_book = max(len(i) for i in len_book)
-        print(f"проверка длины книги = {max_str_book}")
-    return max_str_book
+def title():
+    with open(BOOK_TXT, encoding='utf8') as fn:
+        enum_book = fn.read().splitlines()
+        random_str = random.choice(enum_book)
+    return random_str
 
 
 if __name__ == '__main__':
