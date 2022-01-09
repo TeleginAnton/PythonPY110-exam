87f5f70c6d24038478c832fe2c5e7cac5e893c3e
Anton <91902160+TeleginAnton@users.noreply.github.com>
2022-01-09 12:49:02 +0300

    corrections main.py

--- main.py
+++ main.py
@@ -1,32 +1,40 @@
-import json
 import random
+import json
 
 from faker import Faker
 from pprint import pprint
+from typing import List, Any
 
 from conf import MODEL
 
 BOOK_JSON = "book.json"
 BOOK_TXT = "books.txt"
 
+fake = Faker()
 
-def _main() -> None:
+
+def _main() -> Any:
     """
-    Формирует список из 100 книг (список словарей) и записывает в файл book.json только один словарь :(
+    Формирует список из 100 книг (список словарей) и записывает в файл book.json.
     """
-    python_object = generator()
-    with open(BOOK_JSON, "w") as function:
-        # for word in python_object:
-        #     function.write(word + '\n')
-        json.dump(python_object, function, indent=4, ensure_ascii=False)
+    # pprint([next(generator_book()) for _ in range(5)], sort_dicts=False)
+    json_list = []
+    count = 1
+    for i in generator_book():
+        if count > 15:
+            break
+        json_list += [i]
+        count += 1
+        # pprint(json_list, sort_dicts=False)
+
+    with open(BOOK_JSON, "w", encoding='utf8') as function:
+        json.dump(json_list, function, indent=4, ensure_ascii=False)
 
 
-def pk_():
-    pk = 1
-    while pk <= 100:
-        yield pk
-        pk += 1
-    return pk
+def generator_book(counter: int = 0):
+    while True:
+        counter += 1
+        yield {"model": MODEL, "pk": counter, "fields": fields()}
 
 
 def title() -> str:
@@ -45,8 +53,7 @@ def year() -> int:
     Функция - генератор.
     :return: Год издания - генерируемый объект.
     """
-    num_four_signs = (random.randrange(2000, 2021, 6))
-    return num_four_signs
+    return random.randrange(2000, 2021, 6)
 
 
 def pages() -> int:
@@ -54,8 +61,7 @@ def pages() -> int:
     Функция - генератор.
     :return: Количество страниц - генерируемый объект.
     """
-    num_three_signs = (random.randint(100, 200))
-    return num_three_signs
+    return random.randint(100, 200)
 
 
 def isbn13() -> None:
@@ -63,18 +69,17 @@ def isbn13() -> None:
     Функция - генератор.
     :return: Международный книжный номер - генерируемый объект.
     """
-    fake = Faker()
-    book_num = (fake.numerify(text='%%%-%-%%%%%-%%%-%') for _ in range(5))
-    return next(book_num)
+    for _ in range(5):
+        fake.isbn13()
+    return fake.isbn13()
 
 
-def rating() -> int:
+def rating() -> float:
     """
     Функция - генератор.
     :return: Рейтинг - генерируемый объект.
     """
-    int_nun = (random.randint(0, 5))
-    return int_nun
+    return round(random.uniform(0, 10), 1)
 
 
 def price() -> float:
@@ -82,18 +87,19 @@ def price() -> float:
     Функция - генератор.
     :return: Стоимость книги - генерируемый объект.
     """
-    float_num = float(random.randint(0, 9000))
-    return float_num
+    return round(random.uniform(0, 1500), 2)
 
 
-def author() -> None:
+def author():
     """
     Функция - генератор.
     :return: Автор - генерируемый объект.
     """
-    fake = Faker()
-    one_author = (fake.name())
-    return one_author
+    test = []
+    for _ in range(random.randint(1, 3)):
+        for i in [fake.name()]:
+            test += [i]
+    return test
 
 
 def fields() -> dict:
@@ -101,14 +107,6 @@ def fields() -> dict:
                   "pages": pages(), "isbn13": isbn13(),
                   "rating": rating(), "price": price(), "author": author()}
     return new_fields
-
-
-def generator():
-    model = MODEL
-    for i in pk_():
-        res = {"model": model, "pk": i, "fields": fields()}
-        pprint(res, sort_dicts=False)
-    return res
 
 
 if __name__ == '__main__':
